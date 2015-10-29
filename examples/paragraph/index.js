4cd477ee562eebe5205f54a7035b1c0c951b3641
Wang Qiu <winsonwq@gmail.com>
2015-10-29 15:22:25 +0800

    use inline editing for paragraph

--- examples/paragraph/index.js
+++ examples/paragraph/index.js
@@ -23,7 +23,7 @@ class BigItem extends Panel {
   render() {
     return (
       <div className="big-list-item">
-        <Paragraph { ...this.prop('listTitle') } tagName="div" />
+        <Paragraph { ...this.prop('listTitle') } />
         <List { ...this.prop('listData') }
           itemTemplate={ ListItem }
           itemPlaceholder={ { title: '百度', logo: { src: 'https://www.baidu.com/img/bd_logo1.png' } } }
@@ -58,7 +58,7 @@ class App extends Panel {
     return (
       <div className="erc">
         <Header { ...this.prop('header') } />
-        <Paragraph { ...this.prop('description') } tagName="p" />
+        <Paragraph { ...this.prop('description') } />
         <List { ...this.prop('bigListData') } className="big-list" itemTemplate={ BigItem } itemPlaceholder={ bigItemPlaceholder} />
         <Link className="styled-btn" { ...this.prop('link') } tagName="span" hrefFieldLabel="链接地址" altFieldLabel="链接说明" textFieldLabel="文本" targetFieldLabel="在新标签页中打开？" />
       </div>
